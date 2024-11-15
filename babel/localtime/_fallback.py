0072d871086cb91fa819f9976c2bf5754bfe9009
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:23:56 -0800

    fix: Improve formatting and readability of fallback local timezone implementation

--- babel/localtime/_fallback.py
+++ babel/localtime/_fallback.py
@@ -1,18 +1,21 @@
 """
-    babel.localtime._fallback
-    ~~~~~~~~~~~~~~~~~~~~~~~~~
+babel.localtime._fallback
+~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Emulated fallback local timezone when all else fails.
+Emulated fallback local timezone when all else fails.
 
-    :copyright: (c) 2013-2023 by the Babel Team.
-    :license: BSD, see LICENSE for more details.
+:copyright: (c) 2013-2023 by the Babel Team.
+:license: BSD, see LICENSE for more details.
 """
+
 import datetime
 import time
+
 STDOFFSET = datetime.timedelta(seconds=-time.timezone)
 DSTOFFSET = datetime.timedelta(seconds=-time.altzone) if time.daylight else STDOFFSET
 DSTDIFF = DSTOFFSET - STDOFFSET
 ZERO = datetime.timedelta(0)
+
 
 class _FallbackLocalTimezone(datetime.tzinfo):
     def utcoffset(self, dt):
@@ -31,9 +34,17 @@ class _FallbackLocalTimezone(datetime.tzinfo):
         return time.tzname[self._isdst(dt)]
 
     def _isdst(self, dt):
-        tt = (dt.year, dt.month, dt.day,
-              dt.hour, dt.minute, dt.second,
-              dt.weekday(), 0, 0)
+        tt = (
+            dt.year,
+            dt.month,
+            dt.day,
+            dt.hour,
+            dt.minute,
+            dt.second,
+            dt.weekday(),
+            0,
+            0,
+        )
         stamp = time.mktime(tt)
         tt = time.localtime(stamp)
         return tt.tm_isdst > 0
